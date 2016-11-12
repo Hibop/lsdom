88925ffedbb4806fbdeeea8d500485d2d1d8e231
Liu Shuo <shuo.liu@linecorp.com>
2016-11-12 23:31:01 +0900

    2 http://tech.colla.me/ja/show/a_todo_app_premitive_model&view

--- app.js
+++ app.js
@@ -9,49 +9,112 @@
     };
 
 
+    // Model class
+    function Model(conf){
+        this.data = {};
+        this._listeners = {};
+
+        Object.assign(this, conf);
+    }
+
+    Model.prototype.trigger = function(events, data){
+        var events = events.split(/\W+/);
+        var that = this;
+        events.forEach(function(event){
+            if (that._listeners[event]){
+                that._listeners[event].forEach(function(listener){
+                    listener(data);
+                });
+            }
+        });
+    };
+
+    Model.prototype.listen = function(events, listener){
+        var events = events.split(/\W+/);
+        var that = this;
+        events.forEach(function(event){
+            if (!that._listeners[event]){
+                that._listeners[event] = [listener];
+            } else {
+                that._listeners[event].push(listener);
+            }
+        });
+    };
+
+    // View class
+    function View(conf){
+        Object.assign(this, conf);
+        this.init();
+    };
+
+    View.prototype.clear = function(){
+        this.$dom.parentNode.removeChild(this.$dom);
+    };
+
+    View.prototype.listenTo = function(model, event, listener){
+        model.listen(event, listener);
+    };
+
     // app
-    var $count = document.querySelector('.jsCount');
-    var $list = document.querySelector('.jsList');
-    var $listEmpty = document.querySelector('.jsListEmpty');
-    var $input = document.querySelector('.jsInput');
-    var $add = document.querySelector('.jsAdd');
+    var appModel = new Model({
+        data: [],
+        add: function(item){
+            this.data.push(item);
+            this.trigger('add', item);
+        },
 
-    var data = [];
-
-    var addItem = function(item){
-        data.push(item);
-        var newItem = Render('#js-tmpl-item', item);
-        newItem.querySelector('.jsDelete').addEventListener('click', function(){
-            removeItem(item);
-            $list.removeChild(newItem);
-        }, false);
-        $list.appendChild(newItem);
-    };
-
-    var removeItem = function(item){
-        data.splice(data.indexOf(item), 1);
-        updateCount();
-    };
-
-    var updateCount = function(){
-        $count.innerHTML = data.length;
-
-        if (data.length > 0){
-            $listEmpty.style.display = 'none';
-        } else {
-            $listEmpty.style.display = 'block';
+        remove: function(item){
+            var index = this.data.indexOf(item);
+            this.data.splice(index, 1);
+            this.trigger('delete', index);
         }
-    };
+    });
 
-    var addHandler = function(){
-        var input = $input.value.trim();
-        if (input.length === 0){
-            return;
+    var appView = new View({
+        $dom: document.querySelector('#view'),
+        init: function(){
+            this.$count = this.$dom.querySelector('.jsCount');
+            this.$list = this.$dom.querySelector('.jsList');
+            this.$listEmpty = this.$dom.querySelector('.jsListEmpty');
+            this.$input = this.$dom.querySelector('.jsInput');
+            this.$add = this.$dom.querySelector('.jsAdd');
+
+            this.$add.addEventListener('click', this._onClickAdd.bind(this), false);
+
+            this.listenTo(appModel, 'add', this.add.bind(this));
+            this.listenTo(appModel, 'delete', this.delete.bind(this));
+            this.listenTo(appModel, 'add delete', this.update.bind(this));
+        },
+
+        add: function(item){
+            var newItem = Render('#js-tmpl-item', item);
+            newItem.querySelector('.jsDelete').addEventListener('click', function(){
+                appModel.remove(item);
+            }, false);
+            this.$list.appendChild(newItem);
+        },
+
+        delete: function(index){
+            this.$list.removeChild(this.$list.querySelectorAll('li')[index + 1]);
+        },
+
+        update: function(){
+           this.$count.innerHTML = appModel.data.length;
+
+            if (appModel.data.length > 0){
+                this.$listEmpty.style.display = 'none';
+            } else {
+                this.$listEmpty.style.display = 'block';
+            }
+        },
+
+        _onClickAdd: function(){
+             var input = this.$input.value.trim();
+            if (input.length === 0){
+                return;
+            }
+            appModel.add(input);
+            this.$input.value = '';
         }
-        addItem(input);
-        $input.value = '';
-        updateCount();
-    };
-
-    $add.addEventListener('click', addHandler, false);
+    });
 })();
