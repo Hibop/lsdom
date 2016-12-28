3679bceb7ed038b2cff5e8dba9d6a6d08bbfd4e0
sunderls <sunderls@126.com>
2016-12-28 16:57:36 +0900

    all in directive

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,7 @@
 // directive todo-item
 Directive.create('todo-item', {
     tmpl: '<li>{todo.name}<button click="remove(todo)">x</button></li>',
-    scope: {
+    scopeConfig: {
         todo: '='
     }
 });
@@ -9,7 +9,7 @@ Directive.create('todo-item', {
 // directive add-todo
 Directive.create('add-todo', {
     tmpl: '<input type="text" model="newItemName"><button click="add(newIem)">add</button>',
-    scope: {
+    scopeConfig: {
         newItemName: '',
         addItem: '=',
         add() {
@@ -22,26 +22,37 @@ Directive.create('add-todo', {
     }
 });
 
-// app
-class appController extends Controller {
-    constructor(props) {
-        super(props);
-        Object.assign(this.scope, {
-            todos: [],
-            remove: this.remove.bind(this),
-            addItem: this.add.bind(this)
-        });
-        this.init();
-    }
+
+// directive todo app
+Directive.create('todo-app', {
+    tmpl: `<div>
+            <h1>{'To' + 'DO'}: {todos.length}</h1>
+            <ul>
+                <li style="{display: todos.length > 0 ? 'none' : 'inherit'}">no item</li>
+                <todo-item for="item in todos" todo="item"></todo-item>
+            </ul>
+            <p><add-todo todos="todos" addItem="addItem"></add-todo></p>
+        </div>`,
+    scope: {
+        todos: [],
+        remove: function(item){
+            this.remove(item);
+        },
+        addItem: function(item){
+            this.add(item);
+        }
+    },
 
     remove(item){
         let index = this.scope.todos.indexOf(item);
         this.scope.todos.splice(index, 1);
-    }
+    },
 
     add(item){
         this.scope.todos.push(item);
     }
-}
+});
 
-window.app = new appController();
+
+// init
+Directive.render('todo-app', document.getElementById('app'));
